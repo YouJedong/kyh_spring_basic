2ca33f1f3329906d3035cda96489da62984e404e
mraiou92@gmail.com <mraiou1@naver.com>
2024-01-26 22:43:09 +0900

    first commit

---
+++ src/main/java/com/jedong/jedongspring/JedongSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jedong.jedongspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedongSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedongSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jedong/jedongspring/JedongSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedong.jedongspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedongSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
