25052d57b95a12dc6b72f7687c11fe41bc82e432
YouJedong <https://github.com/YouJedong/kyh_spring_basic>
2024-01-28 22:19:16 +0900

    update: cmd에서 프로젝트 빌드 방법 외 2건
    update: cmd에서 프로젝트 빌드 방법
    update: mvc 기본 배우기

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kyh_spring_basic
 김영한의 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
+
+## 0128
+1. cmd에서 프로젝트 빌드 방법
+    1. gradle 있는 경로까지 cd
+    2. ./gradlew build
+    3. cd build/libs 들어가서 jar파일명 복사
+    4. java -jar {파일명}
+2. mvc 기본 배우기
+   - 섹션 2 > 스프링 웹 개발 기초 > API
+   - Api에 @ResponseBody를 붙어주지 않으면 return할 때 viewResolver를 통해서 템플릿을 찾아 html을 던져줌 [JedongController - 19L]
+   - @ResponseBody를 붙어주면 return할 때 HttpMessageConverter를 통해 String이면 StringConverter 객체면 jsonConverter를 통해 데이터를 그대로 응답 [JedongController - 26L]
--- src/main/java/com/jedong/jedongspring/controller/helloController.java
+++ src/main/java/com/jedong/jedongspring/controller/helloController.java
@@ -3,6 +3,8 @@ package com.jedong.jedongspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
@@ -12,5 +14,19 @@ public class helloController {
         model.addAttribute("data", "hello");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-mvc";
+    }
+
+    @GetMapping("hello-response-body")
+    @ResponseBody
+    public String helloResponseBody(@RequestParam String name) {
+
+        return "hello responseBody - name: " + name;
     }
 }
